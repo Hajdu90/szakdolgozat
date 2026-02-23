8af72df8f90b38790bde932b54219b7b3b1faaab
Hajdu90 <hajdu90shares@gmail.com>
2026-02-23 15:02:02 +0100

    autentikacios_reteg

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,44 +1,8 @@
 <?php
 
-use App\Http\Controllers\FelhsznaloController;
-use App\Http\Controllers\FoglalasokController;
-use App\Http\Controllers\HelyszinController;
-use App\Http\Controllers\KepekController;
-use App\Http\Controllers\LastMArController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\UtazasiCsomagokController;
-use App\Http\Controllers\UtazasiModController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-//User
-Route::get('/users', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-//Utazasok
-Route::get('/utazasi_csomagoks',[UtazasiCsomagokController::class,'index']);
-Route::post('/utazasi-csomagok', [UtazasiCsomagokController::class, 'store']);
-Route::get('/utazasi_csomagoks/{id}', [UtazasiCsomagokController::class,'show']);
-
-
-//Foglalasok
-Route::get('/foglalasok', [FoglalasokController::class, 'index']);
-Route::post('/foglalasok', [FoglalasokController::class, 'store']);
-
-//Users
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-
-//Kepek
-Route::get('/kepeks', [KepekController::class, 'index']);
-Route::post('/kepeks', [KepekController::class, 'index']);
-
-//Helyszinek
-Route::get('/helyszins', [HelyszinController::class, 'index']);
-Route::post('/helyszins', [HelyszinController::class, 'index']);
-
-//Utazasi_modok
-Route::get('/utazasi_mods', [UtazasiModController::class, 'index']);
-Route::post('/utazasi_mods', [UtazasiModController::class, 'index']);
+});
